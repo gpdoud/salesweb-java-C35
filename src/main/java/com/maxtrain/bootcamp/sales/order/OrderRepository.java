4335fcf3dab9560fc09ec32478c778c5d200fc02
Greg Doud <gdoud@gmail.com>
2022-03-09 16:27:09 -0500

    Initial commit

---
+++ src/main/java/com/maxtrain/bootcamp/sales/order/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.maxtrain.bootcamp.sales.order;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface OrderRepository extends CrudRepository<Orders, Integer> {
+
+}
